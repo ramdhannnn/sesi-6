e8ec3ae5ea41dcd25895811090cda8269681261a
ramdhannnn <ramdhan.nurkholik_ti24@nusaputra.ac.id>
2025-03-19 20:58:50 +0700

    Add files via upload

---
+++ deret_1.py
@@ -0,0 +1,9 @@
+result = 50
+subtrack = 2
+
+looping = 8
+
+for i in range(8):
+    print (result,end=' ')
+    result -= subtrack
+    subtrack += 2
---
+++ deret_2.py
@@ -0,0 +1,4 @@
+a , b= 2 , 3
+for i in range(8):
+    print(a,end=' ')
+    a,b = b,a + b 
